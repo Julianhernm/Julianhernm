7225c80a89371bd536ded696db262cb9da564e9a
Julián H. <juliahernandez.pp1@gmail.com>
2025-11-23 17:16:32 -0600

    nuevos cosas

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
-const { join } = require("path")
+const { join } = require("path");
+const cookieParser = require("cookie-parser")
 
 const app = express()
 const PORT = 3000;
@@ -8,6 +9,7 @@ const PORT = 3000;
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(cookieParser())
 
 app.set("view engine", "ejs");
 app.set("viwes", join(__dirname, "workout", "views"));
