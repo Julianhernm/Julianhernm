891f4590dedf062c6c6e620f314924326ba68d3e
Julián H. <juliahernandez.pp1@gmail.com>
2025-11-23 18:58:59 -0600

    nuevo

--- workout/routes/hello.js
+++ workout/routes/hello.js
@@ -1,7 +1,1 @@
-<<<<<<< HEAD
-console.log("hello git 3 v2")
-console.log("este no me gusta")
-console.log("jfasfkd")
-=======
-console.log("hello git 3")
->>>>>>> a9a6 (Login)
+console.log("codigo nuevo")
